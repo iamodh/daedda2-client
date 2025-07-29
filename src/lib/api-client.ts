f3262627c43aa648b3be2f6c52c0e8b7c20179d4
Donghwan Oh <imodh97@gmail.com>
2025-07-29 22:21:09 +0900

    response interceptor 추기

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
 });
+
+api.interceptors.response.use((response) => {
+  return response.data;
+});
