6c59fcd650eec05d889815b2ad7a8725dd2f2545
Donghwan Oh <imodh97@gmail.com>
2025-07-31 11:31:41 +0900

    dropdown storybook 추가

---
+++ src/components/ui/dropdown/dropdown.stories.tsx
@@ -0,0 +1,33 @@
+import { Dropdown } from '@/components/ui/dropdown/dropdown';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { fn } from 'storybook/test';
+
+const ActionsData = {
+  onChange: fn(),
+};
+
+const meta = {
+  component: Dropdown,
+  title: 'Dropdown',
+  args: {
+    ...ActionsData,
+  },
+} satisfies Meta<typeof Dropdown>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const WorkTime: Story = {
+  args: {
+    title: '근무 시간',
+    size: 'md',
+    options: [
+      { name: '초기화', value: 'default' },
+      { name: '0~4시간', value: 'short' },
+      { name: '4~8시간', value: 'medium' },
+      { name: '8시간 초과', value: 'long' },
+    ],
+    value: '초기화',
+    name: 'pay',
+  },
+};
