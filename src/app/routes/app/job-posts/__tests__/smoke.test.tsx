9cb4b3c7d49f07404db3d1aeef2e3f6a36706806
Donghwan Oh <imodh97@gmail.com>
2025-08-23 22:29:57 +0900

    안쓰는 파일 제거

--- src/app/routes/app/job-posts/__tests__/smoke.test.tsx
+++
@@ -1,7 +0,0 @@
-import { it } from 'vitest';
-import JobPostsRoute from '@/app/routes/app/job-posts';
-import { renderApp } from '@/testing/test-utils';
-
-it('JobPostsRoute가 렌더링된다(스모크)', () => {
-  renderApp(<JobPostsRoute />);
-});
