b03042234fa7f930cf88a68730a9b83bbffadbf7
Donghwan Oh <imodh97@gmail.com>
2025-08-25 22:49:46 +0900

    api 호출 sleep 제거, query dev tool 제거

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -27,7 +27,7 @@ export const AppProvider = ({ children }: React.PropsWithChildren) => {
       }
     >
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
+        {/* <ReactQueryDevtools /> */}
         {children}
       </QueryClientProvider>
     </Suspense>
