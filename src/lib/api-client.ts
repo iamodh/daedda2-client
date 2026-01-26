feb262e9ea2dfbed3201fea84177718cd7567c27
Donghwan Oh <imodh97@gmail.com>
2026-01-26 11:52:53 +0900

    chore:api 요청 배포 버전으로 수정

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -2,7 +2,7 @@ import { getDelayState } from '@/store/delay';
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_DEV_API_URL,
+  baseURL: import.meta.env.VITE_APP_API_URL,
 });
 
 api.interceptors.request.use((config) => {
